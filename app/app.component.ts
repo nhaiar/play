bc4b8e68712843ee5dcd65d9de207dc69ed88990
Nick Haiar <nhaiar@unlimitedsystems.com>
2016-07-05 21:43:53 -0400

    Made few updates

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>My First Portfolio Website</h1>
+    <p>{{notes}}</p>`,
 })
-export class AppComponent { }
+export class AppComponent {
+    notes = 'Welcome to the my site!';
+
+}
